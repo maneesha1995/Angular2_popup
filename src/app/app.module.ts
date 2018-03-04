f1cb074d86ce6f54c861305175c2b688a90b5a1e
maneesha1995 <mkwmanisha@gmail.com>
2018-03-04 10:37:19 +0530

    Angular 2 popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import {PopupModule} from 'ng2-opd-popup';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    PopupModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
